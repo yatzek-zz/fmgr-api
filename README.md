499c366ca6c8055b9521a0fcbb6d165be3acd3b9
mysky <dl-cgmysky@bskyb.com>
2014-08-26 21:32:56 +0100

    added new relic

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 TODO:
-
-new relic (https://github.com/yvasiyarov/gorelic)
--- web.go
+++ web.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"github.com/yvasiyarov/gorelic"
 )
 
 func main() {
-	http.HandleFunc("/", hello)
+
+	agent := newRelicAgent()
+	http.HandleFunc("/", agent.WrapHTTPHandlerFunc(handler))
+
 	fmt.Println("listening...")
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
@@ -15,6 +19,17 @@ func main() {
 	}
 }
 
-func hello(res http.ResponseWriter, req *http.Request) {
+// configures new relic agent
+func newRelicAgent() (*gorelic.Agent) {
+	agent := gorelic.NewAgent()
+	agent.Verbose = true
+	agent.NewrelicName = "fmgr-api"
+	agent.CollectHTTPStat = true
+	agent.NewrelicLicense = "4ea69fc41f601a44712b071ab214352bd00087d4"
+	agent.Run()
+	return agent
+}
+
+func handler(res http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(res, "hello, world")
 }
