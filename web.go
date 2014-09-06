e1930cb4d0122d3922b6058c92194b1cdec1f40f
mysky <dl-cgmysky@bskyb.com>
2014-09-07 00:37:49 +0100

    connect to postgres with pg library, use gorp for ORM, players route

--- web.go
+++ web.go
@@ -1,25 +1,79 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
+	"encoding/json"
+	"log"
 	"net/http"
+	"fmt"
 	"os"
+
+	"github.com/coopernurse/gorp"
+	_ "github.com/lib/pq"
 	"github.com/yvasiyarov/gorelic"
 )
 
-func main() {
-	agent := newRelicAgent()
-	http.HandleFunc("/", agent.WrapHTTPHandlerFunc(handler))
+type Player struct {
+	Id      int64  `json:"id"`
+	Name    string `json:"name"`
+	Surname string `json:"surname"`
+	Email   string `json:"email"`
+}
 
-	fmt.Println("listening...")
-	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+func main() {
+	newrelic := newRelicAgent()
+	http.HandleFunc("/players", newrelic.WrapHTTPHandlerFunc(playersHandleFunc))
+
+	port := os.Getenv("PORT")
+	fmt.Println("Listening on port:" + port)
+	err := http.ListenAndServe(":" + port, nil)
 	if err != nil {
 		panic(err)
 	}
 }
 
+func playersHandleFunc(res http.ResponseWriter, req *http.Request) {
+	jzon, err := json.Marshal(getPlayers())
+	checkErr(err, "Serialization of players into json failed")
+	fmt.Fprintln(res, string(jzon))
+}
+
+func getPlayers() []Player {
+	dbmap := initDb()
+	defer dbmap.Db.Close()
+
+	var players []Player
+	_, err := dbmap.Select(&players, "select id, name, surname, email from players order by id")
+	checkErr(err, "Select failed")
+	return players
+}
+
+func initDb() *gorp.DbMap {
+	db, err := sql.Open("postgres", connectionUrl())
+	checkErr(err, "sql.Open failed")
+
+	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.SqliteDialect{}}
+	dbmap.AddTableWithName(Player{}, "players").SetKeys(true, "Id")
+
+	return dbmap
+}
+
+func checkErr(err error, msg string) {
+	if err != nil {
+		log.Fatalln(msg, err)
+	}
+}
+
+func connectionUrl() string {
+	url := os.Getenv("DATABASE_URL")
+	if url == "" {
+		panic("DATABASE_URL environment param na specified, exiting!")
+	}
+	return url + "?sslmode=disable"
+}
+
 // configures new relic agent
-func newRelicAgent() (*gorelic.Agent) {
+func newRelicAgent() *gorelic.Agent {
 	agent := gorelic.NewAgent()
 	agent.Verbose = true
 	agent.NewrelicName = "fmgr-api"
@@ -27,8 +81,4 @@ func newRelicAgent() (*gorelic.Agent) {
 	agent.NewrelicLicense = "6250f7427b4873ef4ece6aba345e4801aa690ec8"
 	agent.Run()
 	return agent
-}
-
-func handler(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(res, "hello, world")
 }
